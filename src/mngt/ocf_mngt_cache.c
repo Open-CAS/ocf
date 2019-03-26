873cc5dc42afe77f2b381fd44b2c4e32de070136
Vitaliy Mysak <vitaliy.mysak@intel.com>
2019-03-26 02:58:50 +0000

    Put cache after doing pipeline_destroy
    Cache is used during pipeline_destroy which means that
      doing put_cache before destroying pipeline may result in
      accessing memory that was freed.
    
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

--- src/mngt/ocf_mngt_cache.c
+++ src/mngt/ocf_mngt_cache.c
@@ -2063,16 +2063,6 @@ static void ocf_mngt_cache_stop_finish(ocf_pipeline_t pipeline,
 	if (error)
 		context->error = error;
 
-	env_mutex_lock(&ctx->lock);
-	/* Mark device uninitialized */
-	cache->valid_ocf_cache_device_t = 0;
-	/* Remove cache from the list */
-	list_del(&cache->list);
-	/* Finally release cache instance */
-	ocf_mngt_cache_put(cache);
-	env_mutex_unlock(&ctx->lock);
-
-
 	if (context->error == -OCF_ERR_WRITE_CACHE) {
 		ocf_log(ctx, log_warn, "Stopped cache %s with errors\n",
 				context->cache_name);
@@ -2087,6 +2077,15 @@ static void ocf_mngt_cache_stop_finish(ocf_pipeline_t pipeline,
 	context->cmpl(cache, context->priv, context->error);
 
 	ocf_pipeline_destroy(context->pipeline);
+
+	env_mutex_lock(&ctx->lock);
+	/* Mark device uninitialized */
+	cache->valid_ocf_cache_device_t = 0;
+	/* Remove cache from the list */
+	list_del(&cache->list);
+	/* Finally release cache instance */
+	ocf_mngt_cache_put(cache);
+	env_mutex_unlock(&ctx->lock);
 }
 
 struct ocf_pipeline_properties ocf_mngt_cache_stop_pipeline_properties = {
