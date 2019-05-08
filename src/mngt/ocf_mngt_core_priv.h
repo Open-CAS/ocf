2ec3681b96c0a1cc07ec339ab32a81b9d2eaff6e
Robert Baldyga <robert.baldyga@intel.com>
2019-05-08 12:38:11 +0200

    Initialize and deinitialize core pool with ctx
    Signed-off-by: Robert Baldyga <robert.baldyga@intel.com>
    (cherry picked from commit 6c7df8abf6bbf2bed18fcdc4ceccea0fbfd8b507)

--- src/mngt/ocf_mngt_core_priv.h
+++ src/mngt/ocf_mngt_core_priv.h
@@ -3,11 +3,11 @@
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
-#ifndef __OCF_CORE_MNGT_PRIV_H__
-#define __OCF_CORE_MNGT_PRIV_H__
+#ifndef __OCF_MNGT_CORE_PRIV_H__
+#define __OCF_MNGT_CORE_PRIV_H__
 
 #include "../ocf_core_priv.h"
 
 int ocf_mngt_core_init_front_volume(ocf_core_t core);
 
-#endif /* __OCF_CORE_MNGT_PRIV_H__ */
+#endif /* __OCF_MNGT_CORE_PRIV_H__ */
