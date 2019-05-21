a671a282828b91d363d15ab77ba4876ee2cc10fa
Robert Baldyga <robert.baldyga@intel.com>
2019-05-21 15:07:02 +0200

    Convert UT scripts to python3
    Signed-off-by: Robert Baldyga <robert.baldyga@intel.com>

--- tests/unit/tests/add_new_test_file.py
+++ tests/unit/tests/add_new_test_file.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 #
 # Copyright(c) 2012-2018 Intel Corporation
@@ -11,7 +11,7 @@ import os
 
 args = ' '.join(sys.argv[1:])
 script_path = os.path.dirname(os.path.realpath(__file__))
-framework_script_path = script_path + os.sep + "../framework/add_new_test_file.py"
+framework_script_path = os.path.join(script_path, "../framework/add_new_test_file.py")
 framework_script_path = os.path.normpath(framework_script_path)
 status, output = commands.getstatusoutput(framework_script_path + " " + args)
 
