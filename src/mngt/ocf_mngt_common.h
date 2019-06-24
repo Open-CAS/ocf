a818ffabfdc389252e83676126c4bfa84acdfa68
Michal Rakowski <michal.rakowski@intel.com>
2019-06-24 15:23:06 +0200

    Move ocf_mngt_cache_is_dirty to public mngt interface

--- src/mngt/ocf_mngt_common.h
+++ src/mngt/ocf_mngt_common.h
@@ -21,8 +21,6 @@ int _ocf_cleaning_thread(void *priv);
 
 int cache_mngt_thread_io_requests(void *data);
 
-bool ocf_mngt_cache_is_dirty(ocf_cache_t cache);
-
 int ocf_mngt_add_partition_to_cache(struct ocf_cache *cache,
 		ocf_part_id_t part_id, const char *name, uint32_t min_size,
 		uint32_t max_size, uint8_t priority, bool valid);
