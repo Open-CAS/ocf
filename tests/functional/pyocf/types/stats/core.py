5f357272d13c59b3e321bf84957bbcf28aaaa94b
Michal Mielewczyk <michal.mielewczyk@intel.com>
2019-09-04 07:37:52 -0400

    pyocf: adjust pyocf to new core stats api
    Signed-off-by: Michal Mielewczyk <michal.mielewczyk@intel.com>

--- tests/functional/pyocf/types/stats/core.py
+++ tests/functional/pyocf/types/stats/core.py
@@ -9,22 +9,13 @@ from ctypes import c_uint32, c_uint64, Structure
 from .shared import OcfStatsReq, OcfStatsBlock, OcfStatsDebug, OcfStatsError
 
 
-class CoreStats(Structure):
+class CoreInfo(Structure):
     _fields_ = [
         ("core_size", c_uint64),
         ("core_size_bytes", c_uint64),
-        ("cache_occupancy", c_uint32),
         ("dirty", c_uint32),
         ("flushed", c_uint32),
         ("dirty_for", c_uint32),
-        ("read_reqs", OcfStatsReq),
-        ("write_reqs", OcfStatsReq),
-        ("cache_volume", OcfStatsBlock),
-        ("core_volume", OcfStatsBlock),
-        ("core", OcfStatsBlock),
-        ("cache_errors", OcfStatsError),
-        ("core_errors", OcfStatsError),
-        ("debug_stat", OcfStatsDebug),
         ("seq_cutoff_threshold", c_uint32),
         ("seq_cutoff_policy", c_uint32),
     ]
