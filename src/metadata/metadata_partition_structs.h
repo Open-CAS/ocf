c0221c6acff1b53d447c976648fc65804c2ac8e2
Michal Rakowski <michal.rakowski@intel.com>
2020-05-04 14:53:10 +0200

    alignment fix
    Signed-off-by: Michal Rakowski <michal.rakowski@intel.com>

--- src/metadata/metadata_partition_structs.h
+++ src/metadata/metadata_partition_structs.h
@@ -11,33 +11,33 @@
 #include "../eviction/eviction.h"
 
 struct ocf_user_part_config {
-        char name[OCF_IO_CLASS_NAME_MAX];
-        uint32_t min_size;
-        uint32_t max_size;
-        int16_t priority;
-        ocf_cache_mode_t cache_mode;
-        struct {
-                uint8_t valid : 1;
-                uint8_t added : 1;
-                uint8_t eviction : 1;
-                        /*!< This bits is setting during partition sorting,
-                         * and means that can evict from this partition
-                         */
-        } flags;
+	char name[OCF_IO_CLASS_NAME_MAX];
+	uint32_t min_size;
+	uint32_t max_size;
+	int16_t priority;
+	ocf_cache_mode_t cache_mode;
+	struct {
+		uint8_t valid : 1;
+		uint8_t added : 1;
+		uint8_t eviction : 1;
+			/*!< This bits is setting during partition sorting,
+			* and means that can evict from this partition
+			*/
+	} flags;
 };
 
 struct ocf_user_part_runtime {
-        uint32_t curr_size;
-        uint32_t head;
-        struct eviction_policy eviction;
-        struct cleaning_policy cleaning;
+	uint32_t curr_size;
+	uint32_t head;
+	struct eviction_policy eviction;
+	struct cleaning_policy cleaning;
 };
 
 struct ocf_user_part {
-        struct ocf_user_part_config *config;
-        struct ocf_user_part_runtime *runtime;
+	struct ocf_user_part_config *config;
+	struct ocf_user_part_runtime *runtime;
 
-        struct ocf_lst_entry lst_valid;
+	struct ocf_lst_entry lst_valid;
 };
 
 
