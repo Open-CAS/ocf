8522b0b6e63fb06bbfdc0248a3339e45431cc512
Jan Musial <jan.musial@intel.com>
2022-02-04 08:15:05 +0100

    Include cache mode and cache line size in metadata probe
    Signed-off-by: Jan Musial <jan.musial@intel.com>

--- src/metadata/metadata.c
+++ src/metadata/metadata.c
@@ -1957,6 +1957,8 @@ static void ocf_metadata_probe_cmpl(struct ocf_metadata_read_sb_ctx *context)
 
 	env_strncpy(status.cache_name, OCF_CACHE_NAME_SIZE, superblock->name,
 			OCF_CACHE_NAME_SIZE);
+	status.cache_mode = superblock->cache_mode;
+	status.cache_line_size = superblock->line_size;
 
 	OCF_CMPL_RET(priv, 0, &status);
 }
