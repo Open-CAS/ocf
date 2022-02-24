bfee654770c124090a2e4166561c092c1073e7bd
Jan Musial <jan.musial@intel.com>
2022-02-24 12:26:02 +0100

    Add wrapper for cache_config_set_default
    Signed-off-by: Jan Musial <jan.musial@intel.com>

---
+++ tests/functional/pyocf/wrappers/ocf_mngt_wrappers.c
@@ -0,0 +1,12 @@
+
+/*
+ * Copyright(c) 2022 Intel Corporation
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "ocf/ocf_mngt.h"
+
+void ocf_mngt_cache_config_set_default_wrapper(struct ocf_mngt_cache_config *cfg)
+{
+	ocf_mngt_cache_config_set_default(cfg);
+}
