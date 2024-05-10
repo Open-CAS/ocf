dc3b581e38aea49c4819e95691fd3d7e7a2464c2
Robert Baldyga <robert.baldyga@huawei.com>
2024-05-11 00:00:19 +0200

    Move declaration to the right header
    Signed-off-by: Robert Baldyga <robert.baldyga@huawei.com>

--- src/concurrency/ocf_cache_line_concurrency.h
+++ src/concurrency/ocf_cache_line_concurrency.h
@@ -1,5 +1,6 @@
 /*
  * Copyright(c) 2012-2021 Intel Corporation
+ * Copyright(c) 2024 Huawei Technologies
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
@@ -110,23 +111,6 @@ void ocf_req_unlock_rd(struct ocf_alock *c,
  */
 void ocf_req_unlock(struct ocf_alock *c,
 		struct ocf_request *req);
-
-/**
- * @Check if cache line is used.
- *
- * Cache line is used when:
- * 1. It is locked for write or read access
- * or
- * 2. There is set locked bit in metadata
- *
- * @param cache - OCF cache instance
- * @param line - Cache line to be unlocked
- *
- * @retval true - cache line is used
- * @retval false - cache line is not used
- */
-bool ocf_cache_line_is_used(struct ocf_alock *c,
-		ocf_cache_line_t line);
 
 /**
  * @brief Check if for specified cache line there are waiters
