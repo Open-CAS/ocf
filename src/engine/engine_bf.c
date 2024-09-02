8fbc8c4201f223d05f2ff2cd6fc486c518f4d49c
Michal Mielewczyk <michal.mielewczyk@huawei.com>
2024-09-02 15:16:34 +0200

    engine_bf: don't backfill read hits
    Signed-off-by: Michal Mielewczyk <michal.mielewczyk@huawei.com>

--- src/engine/engine_bf.c
+++ src/engine/engine_bf.c
@@ -70,13 +70,88 @@ static void _ocf_backfill_complete(struct ocf_request *req, int error)
 
 static int _ocf_backfill_do(struct ocf_request *req)
 {
+	ocf_cache_t cache = req->cache;
+	uint64_t addr, bytes, total_bytes = 0, addr_next = 0;
+	uint64_t seek, skip;
+	uint32_t i;
+
 	req->data = req->cp_data;
 	if (unlikely(req->data == NULL)) {
 		_ocf_backfill_complete(req, -OCF_ERR_NO_MEM);
 		return 0;
 	}
 
-	ocf_engine_forward_cache_io_req(req, OCF_WRITE, _ocf_backfill_complete);
+	req->cache_forward_end = _ocf_backfill_complete;
+
+	if (ocf_engine_is_sequential(req)) {
+		addr = cache->device->metadata_offset;
+		addr += req->map[0].coll_idx * ocf_line_size(cache);
+		addr += req->addr % ocf_line_size(cache);
+
+		ocf_core_stats_cache_block_update(req->core, req->part_id,
+				OCF_WRITE, req->bytes);
+
+		ocf_req_forward_cache_io(req, OCF_WRITE, addr, req->bytes,
+				req->offset);
+		return 0;
+	}
+
+	ocf_req_forward_cache_get(req);
+	for (i = 0; i < req->core_line_count; i++) {
+		if (addr_next) {
+			addr = addr_next;
+		} else {
+			addr  = req->map[i].coll_idx;
+			addr *= ocf_line_size(cache);
+			addr += cache->device->metadata_offset;
+		}
+		bytes = ocf_line_size(cache);
+
+		if (i == 0) {
+			seek = req->addr % ocf_line_size(cache);
+			addr += seek;
+			bytes -= seek;
+		}
+
+		if (req->map[i].status == LOOKUP_HIT) {
+			/* This is the 1st cache line in the interval,
+			 * and it's a hit. Don't write it to the cache.
+			 */
+			addr_next = 0;
+			total_bytes += bytes;
+			continue;
+		}
+
+		for (; i < (req->core_line_count - 1); i++) {
+			addr_next = req->map[i + 1].coll_idx;
+			addr_next *= ocf_line_size(cache);
+			addr_next += cache->device->metadata_offset;
+
+			if (addr_next != (addr + bytes))
+				break;
+
+			bytes += ocf_line_size(cache);
+		}
+
+		if (i == (req->core_line_count - 1)) {
+			skip = (ocf_line_size(cache) -
+					((req->addr + req->bytes) %
+					 ocf_line_size(cache))) % ocf_line_size(cache);
+			bytes -= skip;
+		}
+
+		bytes = OCF_MIN(bytes, req->bytes - total_bytes);
+
+		ocf_core_stats_cache_block_update(req->core, req->part_id,
+				OCF_WRITE, bytes);
+
+		ocf_req_forward_cache_io(req, OCF_WRITE, addr, bytes,
+				req->offset + total_bytes);
+
+		total_bytes += bytes;
+	}
+
+	ocf_req_forward_cache_put(req);
 
 	return 0;
 }
